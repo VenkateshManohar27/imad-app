5d8321ffe99f44afe5b131476167dc2fd2b82c8c
VenkateshManohar27 <venkatesh.manohar@gmail.com>
2017-08-14 11:45:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,6 +75,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter =0;
+
+app.get('/counter',function (req, res){
+    counter = counter + 1;
+    res.send(counter);
+});
+
 app.get('/:articlepar', function(req,res){
     
     var articleObj = req.params.articlepar;
