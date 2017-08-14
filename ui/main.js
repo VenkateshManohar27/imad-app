d31a7407f6c70a375d8dd12120b60fafa7843d66
VenkateshManohar27 <venkatesh.manohar@gmail.com>
2017-08-14 11:54:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,5 @@
 console.log('Loaded!');
-var element = document.getElementById('madi');
 
-var marginLeft = 0;
-function moveRight(){
-    
-    marginLeft = marginLeft+1;
-    element.style.marginLeft = marginLeft+'px';
-}
-
-element.onclick = function(){
-   var interval =  setInterval(moveRight,50);
-}
 
 var buttonEle = document.getElementById('counter');
 var counter = 0;
@@ -21,3 +10,16 @@ buttonEle.onclick = function(){
     
 }
 
+
+/*var element = document.getElementById('madi');
+
+var marginLeft = 0;
+function moveRight(){
+    
+    marginLeft = marginLeft+1;
+    element.style.marginLeft = marginLeft+'px';
+}
+
+element.onclick = function(){
+   var interval =  setInterval(moveRight,50);
+}*/
