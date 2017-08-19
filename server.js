c37acf452abb1cedd22c465e31bcb9d3d947ed03
VenkateshManohar27 <venkatesh.manohar@gmail.com>
2017-08-19 18:45:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,9 +92,9 @@ app.get('/test-db', function (req, res){
 });
 
 function hash(input, salt){
-    var hashed = crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     
-    return hashed.toString('hex');
+    return ['pbkdf2Sync',salt,10000,hashed.toString('hex')].join('$');
 }
 app.get('/hash/:input', function (req, res){
     
