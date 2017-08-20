dab47121da0ef79bbc483185600ea79f31ccb519
VenkateshManohar27 <venkatesh.manohar@gmail.com>
2017-08-20 12:46:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,6 +27,31 @@ buttonEle.onclick = function(){
 var submitBtn = document.getElementById('submit_btn');
 
 submitBtn.onclick=function(){
+    var username = document.getElementById('name').value;
+    var password = document.getElementById('password').value;
+    var request = new XMLHttpRequest();
+   
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status===200){
+                alert('User logged in Successfully');
+                
+            }else if(request.status===403){
+                alert('Username/password is incorrect');
+            }else if (request.status===500){
+                alert('Something went wrong with the server');
+            }
+        }
+    };
+    
+    request.open('POST',"http://venkateshmanohar.imad.hasura-app.io/login", true)
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({"username":username, "password":passwword}));
+    
+};
+
+/*
+submitBtn.onclick=function(){
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
     var request = new XMLHttpRequest();
@@ -49,8 +74,7 @@ submitBtn.onclick=function(){
     request.open('GET',"http://venkateshmanohar.imad.hasura-app.io/submit-btn?name="+name, true)
     request.send(null);
     
-};
-
+};*/
 
 /*submitBtn.onclick=function(){
     var nameList =['Name 1','Name 2','Name 3'];
